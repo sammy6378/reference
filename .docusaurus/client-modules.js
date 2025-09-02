47ccb4cbc6dad0a6e659494dc64d3a7dfac4761f
sammy6378 <samuel202mwangi@gmail.com>
2025-09-02 12:51:24 +0300

    Remove .docusaurus from tracking

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\samue\\Desktop\\All\\CSK\\reference\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\samue\\Desktop\\All\\CSK\\reference\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\samue\\Desktop\\All\\CSK\\reference\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\samue\\Desktop\\All\\CSK\\reference\\src\\css\\custom.css"),
-];
